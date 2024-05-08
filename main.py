c1656e4ed3683e823f047639ebdf87099d51c9de
franpf <franperezfolgado@outlook.com>
2024-05-08 13:50:30 +0200

    Added creation of playlist

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import json
 import random
 from genres import GENRES
 
-SCOPE = 'user-top-read'
+SCOPE = 'user-top-read playlist-modify-public playlist-modify-private'
 
 
 
 def create_connection():
-    
     return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
 
 
 def get_current_top_artist(sp):
-    return sp.current_user_top_artists(limit=10)
+    return sp.current_user_top_artists(limit=30)
 
 def obtain_genres(data):
     genres = [genre for artist in data['items'] for genre in artist['genres']]
@@ -25,19 +24,29 @@ def find_tracks(sp: spotipy.Spotify,genres):
     uris = []
     for genre in random_genres:
         query = f'genre:{genre}'
-        songs = sp.search(limit=1, q=query, type='track')
+        songs = sp.search(limit=5, q=query, type='track')
         uri = [item['uri'] for item in songs['tracks']['items']]
-        uris.append(uri)
+        uris.extend(uri)
     return uris
 
 
-    
-        
-# get random genres
-# find 10 songs per genre
-# acummulate uris in list
-# create playlist
-#add songs using uris
+def create_playlist(sp: spotipy.Spotify):
+    user_details = sp.me()
+    user_name = sp.me()['display_name']
+    playlist_name = f'Randomlist for {user_name}'
+    playlist = sp.user_playlist_create(
+        user=user_details['id'],
+        name=playlist_name,
+        public=True,
+        collaborative=False,
+        description='Playlist created by Franjelou'
+    )
+    return playlist
+
+def add_songs_to_playlist(sp: spotipy.Spotify, uris, playlist):
+    sp.playlist_add_items(
+        items=uris,
+        playlist_id=playlist['id'])
 
 def main():
     sp = create_connection()
@@ -45,7 +54,8 @@ def main():
     listened_genres = obtain_genres(top_artists)
     not_listened_genres = [genre for genre in GENRES if genre not in listened_genres]
     uris = find_tracks(sp, not_listened_genres)
-    print(uris)
+    playlist = create_playlist(sp)
+    add_songs_to_playlist(sp, uris=uris, playlist=playlist)
     
 
 
