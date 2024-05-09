4d507a3b13855850cf56d54a83177d1f036b57ac
franpf <franperezfolgado@outlook.com>
2024-05-10 00:51:17 +0200

    added dockerfile

--- api.py
+++ api.py
@@ -1,8 +1,21 @@
 from flask import Flask
-import randomplaylist
+from recommendations import Recommendation
+
 app = Flask(__name__)
 
+class Api:
+    def __init__(self):
+        self.recommendation = Recommendation()
+    
+    @app.route("/")
+    def test():
+        return '<p>Working</p>'
 
-@app.route("/random-recommendations")
-def random_recommendations():
-    return f'<p>{randomplaylist.generateRandomPlaylist()}</p>'
+    @app.route("/random-recommendations")
+    def random_recommendations():
+        rp = Recommendation()
+        return f'<p>{rp.generate_random_playlist()}</p>'
+
+if __name__ == '__main__':
+    app = Api()
+    app.run(debug=True)
--- recommendations.py
+++ recommendations.py
@@ -3,25 +3,23 @@ from spotipy.oauth2 import SpotifyOAuth
 import random
 from genres import GENRES
 
-class Recomendation:
+class Recommendation:
 
-    SCOPE = 'user-top-read playlist-modify-public playlist-modify-private'
+    def __init__(self):
+        self.scope = 'user-top-read playlist-modify-public playlist-modify-private'
 
-    def __init__(self) -> None:
-        pass
-
-    def __create_connection(self):
-        return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=self.SCOPE))
+    def create_connection(self):
+        return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=self.scope))
 
 
-    def __get_current_top_artist(sp):
+    def get_current_top_artist(self,sp):
         return sp.current_user_top_artists(limit=30)
 
-    def __obtain_genres(data):
+    def obtain_genres(self,data):
         genres = [genre for artist in data['items'] for genre in artist['genres']]
         return [genre for words in genres for genre in words.split()]
 
-    def __find_tracks(sp: spotipy.Spotify,genres):
+    def find_tracks(self, sp: spotipy.Spotify,genres):
         random_genres = random.sample(genres, 15)
         uris = []
         for genre in random_genres:
@@ -32,7 +30,7 @@ class Recomendation:
         return uris
 
 
-    def __create_playlist(sp: spotipy.Spotify):
+    def create_playlist(self,sp: spotipy.Spotify):
         user_details = sp.me()
         user_name = sp.me()['display_name']
         playlist_name = f'Randomlist for {user_name}'
@@ -45,17 +43,19 @@ class Recomendation:
         )
         return playlist
 
-    def __add_songs_to_playlist(sp: spotipy.Spotify, uris, playlist):
+    def add_songs_to_playlist(self,sp: spotipy.Spotify, uris, playlist):
         sp.playlist_add_items(
             items=uris,
             playlist_id=playlist['id'])
 
-    def generate_randm_playlist(self):
+    def generate_random_playlist(self):
         sp = self.create_connection()
         top_artists = self.get_current_top_artist(sp)
         listened_genres = self.obtain_genres(top_artists)
         not_listened_genres = [genre for genre in GENRES if genre not in listened_genres]
         uris = self.find_tracks(sp, not_listened_genres)
         playlist = self.create_playlist(sp)
+        print(playlist)
         self.add_songs_to_playlist(sp, uris=uris, playlist=playlist)
+        return playlist['external_urls']['spotify']
         
